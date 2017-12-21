566c9a972c419166ae4206b3e1d364f06185c995
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2017-12-20 20:43:36 -0600

    document ready

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,11 +1,13 @@
-var qs = queryString.parse(location.search),
-  $body = $(document.body);
-if (qs.script) {
-  $body.append($("<div>").append($("<p>")
-    .html("Drag this to your bookmarks bar: ").append(
-      $("<a>").html(qs.name || "bookmarklet").attr("href", qs.script)
-    )
-  ))
-} else {
-  $body.append("<p>I'm tired, wait pls</p>");
-}
+$(function() {
+  var qs = queryString.parse(location.search),
+    $body = $(document.body);
+  if (qs.script) {
+    $body.append($("<div>").append($("<p>")
+      .html("Drag this to your bookmarks bar: ").append(
+        $("<a>").html(qs.name || "bookmarklet").attr("href", qs.script)
+      )
+    ))
+  } else {
+    $body.append("<p>I'm tired, wait pls</p>");
+  }
+});
