e759221d8564723c3c0d59fd67aa8573525864bc
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2017-12-20 22:28:31 -0600

    alright maybe

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -4,7 +4,7 @@ $(function() {
   if (qs.script) {
     $body.append($("<div>").append($("<p>")
       .html("Drag this to your bookmarks bar: ").append(
-        $("<a>").html(qs.name || "bookmarklet").attr("href", "javascript:" + encodeURIComponent("(function(){if (!window.BMLoader){var s = document.createElement(\"script\");s.src=\"https://cdn.rawgit.com/coolreader18/bookmarklet-loader/v1.1.0/bookmarklet.min.js\";document.body.append(s);s.onload = callback;} else {callback();}function callback() {BMLoader.load" + (qs.github == undefined ? "Bookmarklet" : "Github") + "(\"" + qs.script + \");}})()"));
+        $("<a>").html(qs.name || "bookmarklet").attr("href", "javascript:" + encodeURIComponent("(function(){if (!window.BMLoader){var s = document.createElement(\"script\");s.src=\"https://cdn.rawgit.com/coolreader18/bookmarklet-loader/v1.1.0/bookmarklet.min.js\";document.body.append(s);s.onload = callback;} else {callback();}function callback() {BMLoader.load" + (qs.github === undefined ? "Bookmarklet" : "Github") + "(\"" + qs.script + "\");}})()"));
       )
     ))
   } else {
