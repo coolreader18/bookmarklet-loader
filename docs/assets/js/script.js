92ab75586d07b953ef2882dabfdc2c23dcf06f45
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2017-12-20 20:48:12 -0600

    javascript:

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -4,7 +4,7 @@ $(function() {
   if (qs.script) {
     $body.append($("<div>").append($("<p>")
       .html("Drag this to your bookmarks bar: ").append(
-        $("<a>").html(qs.name || "bookmarklet").attr("href", qs.script)
+        $("<a>").html(qs.name || "bookmarklet").attr("href", "javascript:" + qs.script)
       )
     ))
   } else {
