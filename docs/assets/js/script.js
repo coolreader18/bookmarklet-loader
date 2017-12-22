51b9ca79fa10656072ac97a8c8833f014e2cece0
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2017-12-22 14:40:10 -0600

    Update script.js

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,17 +1,13 @@
-(function() {
-  var qs = queryString.parse(location.search),
-    $body = $(document.body);
-  if (qs.script) {
-    $body.append($("<div>").append($("<p>")
-      .html("Drag this to your bookmarks bar: ").append(
-        $("<a>").html(qs.name || "bookmarklet").attr("href", "javascript:" + encodeURIComponent("(function(){if (!window.BMLoader){var s = document.createElement(\"script\");s.src=\"https://cdn.rawgit.com/coolreader18/bookmarklet-loader/v1.1.2/bookmarklet.min.js\";document.body.append(s);s.onload = callback;} else {callback();}function callback() {BMLoader.load" + (qs.github === undefined ? "Bookmarklet" : "Github") + "(\"" + qs.script + "\");}})()"))
-      )
-    ))
-  } else {
-    $body.append(
-      $("<div>")
-      .append("<h2>Bookmarklets</h2>")
-      .append("<a href='https://github.com/coolreader18/bookmarklet-loader/'>What is this?</a>")
-      .append("<p>You've made a bookmarklet with the bookmarklet loader. Cool! Now, how to distribute it? Well, with this website!\nSimply put in the information, and get out a url that you can link people to!</p>"))
-  }
-})();
+var qs = queryString.parse(location.search);
+if (qs.script) {
+  $("#bookmarklet-link")
+    .text(qs.name || "bookmarklet")
+    .attr("href", "javascript:" + encodeURIComponent(`(function(){if(!window.BMLoader){var s=document.createElement("script");s.src="https://cdn.rawgit.com/coolreader18/bookmarklet-loader/v1.1.2/bookmarklet.min.js";document.body.append(s);s.onload=c}else{c()}function c(){BMLoader.load${qs.github === undefined ? "Bookmarklet" : "Github"}(${qs.script}");}})()`));
+  $("#user").show();
+} else {
+  $("#dev").show()
+  $("#create").click(function() {
+    $("#result").text(`https://coolreader18.github.io/bookmarklet?name=${$("#name").text()}&script=${$("#url").text()}&github`);
+  });
+}
+
