0e5e69681e8e09882a64461f54cf1a2bdf042d0b
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2017-12-22 17:34:57 -0600

    fixed

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -2,7 +2,7 @@ var qs = queryString.parse(location.search);
 if (qs.name&&qs.method&&qs.script) {
   $("#bookmarklet-link")
     .text(qs.name || "bookmarklet")
-    .attr("href", "javascript:" + encodeURIComponent(`(function(){if(!window.BMLoader){var s=document.createElement("script");s.src="https://cdn.rawgit.com/coolreader18/bookmarklet-loader/v1.1.2/bookmarklet.min.js";document.body.append(s);s.onload=c}else{c()}function c(){BMLoader.${qs.method}(${qs.script}");}})()`));
+    .attr("href", "javascript:" + encodeURIComponent(`(function(){if(!window.BMLoader){var s=document.createElement("script");s.src="https://cdn.rawgit.com/coolreader18/bookmarklet-loader/v1.1.2/bookmarklet.min.js";s.onload=c;document.body.append(s)}else{c()}function c(){BMLoader.${qs.method}("${qs.script}");}})()`));
   $("#user").show();
 } else {
   $("input[name=method]:radio").change(e => {
