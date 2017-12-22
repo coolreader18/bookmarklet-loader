39e658c7d88609a98eaed99e71b4c783d73028d7
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2017-12-22 16:26:25 -0600

    Update script.js

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,16 +1,16 @@
 var qs = queryString.parse(location.search);
-if (qs.script) {
+if (qs.name&&qs.method&&qs.script) {
   $("#bookmarklet-link")
     .text(qs.name || "bookmarklet")
-    .attr("href", "javascript:" + encodeURIComponent(`(function(){if(!window.BMLoader){var s=document.createElement("script");s.src="https://cdn.rawgit.com/coolreader18/bookmarklet-loader/v1.1.2/bookmarklet.min.js";document.body.append(s);s.onload=c}else{c()}function c(){BMLoader.load${qs.method.charAt(0).toUpperCase() + qs.method.slice(1)}(${qs.script}");}})()`));
+    .attr("href", "javascript:" + encodeURIComponent(`(function(){if(!window.BMLoader){var s=document.createElement("script");s.src="https://cdn.rawgit.com/coolreader18/bookmarklet-loader/v1.1.2/bookmarklet.min.js";document.body.append(s);s.onload=c}else{c()}function c(){BMLoader.${qs.method}(${qs.script}");}})()`));
   $("#user").show();
 } else {
   $("input[name=method]:radio").change(e => {
-    $("label[for=script]").text(e.currentTarget.value);
+    $("label[for=script]").text($(e.currentTarget).data("type"));
   }).change();
   $("#dev").show();
   $("#create").click(function() {
-    $("#result").text(`Link to ${$("#name").val()}`).attr("href",`https://coolreader18.github.io/bookmarklet-loader?name=${encodeURIComponent($("#name").val())}&script=${encodeURIComponent($("#script").val())}&method=${$("input[name=method]:radio:checked").attr("id")}`).attr("target","bmlinktest");
+    $("#result").text(`Link to ${$("#name").val()}`).attr("href",`https://coolreader18.github.io/bookmarklet-loader?name=${encodeURIComponent($("#name").val())}&script=${encodeURIComponent($("#script").val())}&method=${$("input[name=method]:radio:checked").data("function")}`).attr("target","bmlinktest");
   });
 }
 
