7e115b0f4bfb627074fca4e6df7880ce22a876a8
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2017-12-22 15:32:33 -0600

    Update script.js

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -5,7 +5,10 @@ if (qs.script) {
     .attr("href", "javascript:" + encodeURIComponent(`(function(){if(!window.BMLoader){var s=document.createElement("script");s.src="https://cdn.rawgit.com/coolreader18/bookmarklet-loader/v1.1.2/bookmarklet.min.js";document.body.append(s);s.onload=c}else{c()}function c(){BMLoader.load${qs.github === undefined ? "Bookmarklet" : "Github"}(${qs.script}");}})()`));
   $("#user").show();
 } else {
-  $("#dev").show()
+  $("input[name=method]:radio").change(e => {
+    $("label[for='url']").text(this.value);
+  }).change();
+  $("#dev").show();
   $("#create").click(function() {
     $("#result").text(`https://coolreader18.github.io/bookmarklet?name=${$("#name").text()}&script=${$("#url").text()}&github`);
   });
