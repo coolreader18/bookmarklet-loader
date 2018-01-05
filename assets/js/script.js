e8b2046fa3f9dc7c3367cd3c87804a314155393f
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-01-05 15:55:08 -0600

    fixed stuff up, i'm pretty happy about this now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,55 @@
+---
+---
 var qs = {};
 for (pair of new URL(location.href).searchParams.entries()) {
   qs[pair[0]] = pair[1];
 };
-if (qs.name && qs.method && qs.script) {
-  $("#bookmarklet-link")
-    .text(qs.name)
-    .attr("href", `javascript:${encodeURIComponent(`{let c=()=>{BMLoader.${qs.method}("${qs.script}")};if(window.BMLoader){c()}else{let s=document.createElement("script");s.src="https://cdn.rawgit.com/coolreader18/bookmarklet-loader/v2.1.0/bookmarklet.min.js";document.body.append(s);s.onload=c}}`)}`);
-  $("#user").show();
+if (qs.name && qs.method && qs.script && qs.ver) {
+  (async () => {
+    if (qs.ver == 'latest') {
+      qs.ver = (await $.getJSON("https://api.github.com/repos/coolreader18/bookmarklet-loader/releases/latest")).tag_name
+    }
+    $("#bookmarklet-link")
+    .text(qs.name)
+    .attr("href", `javascript:${encodeURIComponent(`{let c=()=>{BMLoader.${qs.method}("${qs.script}")};if(window.BMLoader){c()}else{let s=document.createElement("script");s.src="https://cdn.rawgit.com/coolreader18/bookmarklet-loader/${qs.ver}/bookmarklet.min.js";document.body.append(s);s.onload=c}}`)}`);
+    $("#user").show();
+  })()
 } else {
-  $("input[name=method]:radio").change(e => {
-    var checked = $("input[name=method]:radio:checked");
-    $("label[for=script]").text(checked.data("type"));
-    $("#script").attr("placeholder", checked.data("placeholder"))
+  $.getJSON("https://api.github.com/repos/coolreader18/bookmarklet-loader/releases")
+  .done(json => {
+    $("#version").material_select("destroy");
+    var group;
+    $("#version").append(group = $("<optgroup label='Individual Versions'>"));
+    json.forEach(rel => {
+      group.append($("<option>").val(rel.tag_name).text(rel.tag_name));
+    })
+    $("#version").material_select();
+  });
+  $("#method").change(e => {
+    var methods = {{ site.data.methods | replace: "=>", ":" }};
+    for (m of methods) {
+      if (m.label.toLowerCase() == e.currentTarget.value.toLowerCase()) {
+        curmethod = m;
+        break;
+      }
+    }
+    $("#script").attr("placeholder", curmethod.placeholder)
+    $("label[for=script]").text(curmethod.type)
   }).change();
+  $(() => {
+    $(".select").material_select()
+  })
   $("#dev").show()
   $("#create").click(function() {
-    $("#res-link").text($("#name").val()).attr("href",`https://coolreader18.github.io/bookmarklet-loader?name=${encodeURIComponent($("#name").val())}&script=${encodeURIComponent($("#script").val())}&method=${$("input[name=method]:radio:checked").data("function")}`).attr("target","bmlinktest");
-    $("#result").show();
+    if ($("#name:valid").length && $("#script:valid").length) {
+      var url = new URL("https://coolreader18.github.io/bookmarklet-loader"),
+      params = url.searchParams;
+      params.set("name", encodeURIComponent($("#name").val()));
+      params.set("script", encodeURIComponent($("#script").val()));
+      params.set("method", curmethod.function);
+      params.set("ver", $("#version").val());
+      $("#res-link").text(encodeURIComponent($("#name").val())).attr("href", '?' + params.toString()).attr("target","bmlinktest");
+      $("#result").show();
+    }
   });
 }
