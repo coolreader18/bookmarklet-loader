613ff75feaaf5df9545938be5485139e5ce47926
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-01-05 09:54:18 +0200

    fixed iterator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var qs = {};
-new URL(location.href).searchParams.entries().forEach(() => {
+for (let pair in new URL(location.href).searchParams.entries()) {
   qs[pair[0]] = pair[1];
 });
 if (qs.name && qs.method && qs.script) {
