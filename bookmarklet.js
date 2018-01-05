d9c5c6747600c483d032c0ec567281bd40ab0006
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-01-05 09:32:11 +0200

    beautified and fixed the eval

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,32 +1,21 @@
 BMLoader = {
   scripts: {},
   parseFile: (data, providedmd) => {
-    var inMetadataBlock = false,
-    openMetadata = '==Bookmarklet==',
-    closeMetadata = '==/Bookmarklet==',
-    rComment = /^(\s*\/\/\s*)/,
-    md = {
-      name: '',
-      version: '',
-      description: '',
-      repository: '',
-      author: '',
-      email: '',
-      url: '',
-      license: '',
+    var inMetadataBlock = false, openMetadata = "==Bookmarklet==", closeMetadata = "==/Bookmarklet==", rComment = /^(\s*\/\/\s*)/, md = {
+      name: "",
+      version: "",
+      description: "",
+      repository: "",
+      author: "",
+      email: "",
+      url: "",
+      license: "",
       script: [],
       style: []
-    },
-    options = {},
-    code = [],
-    errors = [],
-    bookmarklet = false;
-
+    }, options = {}, code = [], errors = [], bookmarklet = false;
     data.match(/[^\r\n]+/g).forEach((line, i, lines) => {
       if (rComment.test(line)) {
-        var comment = line.replace(rComment, '').trim(),
-        canonicalComment = comment.toLowerCase().replace(/\s+/g, '');
-
+        var comment = line.replace(rComment, "").trim(), canonicalComment = comment.toLowerCase().replace(/\s+/g, "");
         if (!inMetadataBlock) {
           if (canonicalComment == openMetadata.toLowerCase()) {
             inMetadataBlock = true;
@@ -38,8 +27,7 @@ BMLoader = {
           } else {
             var m = comment.match(/^@([^\s]+)\s+(.*)$/);
             if (m) {
-              var k = m[1],
-              v = m[2];
+              var k = m[1], v = m[2];
               if (k) {
                 if (Array.isArray(md[k])) {
                   options[k] = options[k] || [];
@@ -56,33 +44,26 @@ BMLoader = {
       } else {
         code.push(line);
       }
-
       if (inMetadataBlock && i + 1 == lines.length) {
         errors.push(`missing metdata block closing \`${closeMetadata}\``);
       }
     });
     return {
       metadata: Object.assign(options, providedmd),
-      code: code.join('\n'),
+      code: code.join("\n"),
       errors: errors.length ? errors : null,
       bookmarklet: bookmarklet
     };
   },
   processScript: async (scripttext, providedmd) => {
-    var parsed = BMLoader.parseFile(scripttext, providedmd),
-    meta = parsed.metadata,
-    code = parsed.code,
-    waitScript = new Promise(async resolveAll => {
+    var parsed = BMLoader.parseFile(scripttext, providedmd), meta = parsed.metadata, code = parsed.code, waitScript = new Promise(async resolveAll => {
       if (!meta.script) {
-        resolveAll()
+        resolveAll();
       } else {
         meta.script.forEach(async (cur, i, scripts) => {
-          var toload = cur,
-          split = cur.split(" "),
-          md;
+          var toload = cur, split = cur.split(" "), md;
           if (split[0] == "dir") {
-            await BMLoader.getGithub("coolreader18/bookmarklet-loader/depend-dir-scripts.min.json")
-            .then(dirurl => fetch(dirurl)).then(unpdir => unpdir.json()).then(dir => {
+            await BMLoader.getGithub("coolreader18/bookmarklet-loader/depend-dir-scripts.min.json").then(dirurl => fetch(dirurl)).then(unpdir => unpdir.json()).then(dir => {
               var script = dir[split[1].toLowerCase()];
               toload = script.url.replace(/%version/, split[2] || script.latest);
               md = script.md;
@@ -97,65 +78,58 @@ BMLoader = {
     });
     if (meta.style) {
       meta.style.forEach(async cur => {
-        var toload = cur,
-        split = cur.split(" "),
-        l = document.createElement("link");
+        var toload = cur, split = cur.split(" "), l = document.createElement("link");
         l.rel = "stylesheet";
         l.href = toload;
         if (split[0] == "dir") {
-          await BMLoader.getGithub("coolreader18/bookmarklet-loader/depend-dir-styles.min.json")
-          .then(dirurl => fetch(dirurl)).then(unpdir => unpdir.json()).then(dir => {
+          await BMLoader.getGithub("coolreader18/bookmarklet-loader/depend-dir-styles.min.json").then(dirurl => fetch(dirurl)).then(unpdir => unpdir.json()).then(dir => {
             var style = dir[split[1].toLowerCase()];
             toload = style.url.replace(/%version/, split[2] || style.latest);
           });
         }
         document.head.append(l);
-      })
+      });
     }
     Object.assign(parsed, providedmd);
     await waitScript;
     if (parsed.bookmarklet) {
       var namespace = BMLoader.scripts[meta.name] = BMLoader.scripts[meta.name] || parsed;
       namespace.clicks = namespace.clicks + 1 || 0;
-      eval(`(() => {${code}})`).call(namespace);
+      eval(`(function(){${code}})`).call(namespace);
     } else {
       eval(code);
     }
     return;
   },
   loadBookmarklet: (script, md) => {
-    return fetch(script)
-    .then(response => {
+    return fetch(script).then(response => {
       if (response.ok) {
-        return response.text()
+        return response.text();
       } else {
         throw new Error("Couldn't load the bookmarklet");
       }
-    }).then(js => {BMLoader.processScript(js, md)})
-    .catch(alert);
+    }).then(js => {
+      BMLoader.processScript(js, md);
+    }).catch(alert);
   },
   parseGithub: file => {
     var filearr = file.split("/");
     return {
       slug: filearr.slice(0, 2).join("/"),
       filepath: filearr.slice(2).join("/")
-    }
+    };
   },
   getGithub: file => {
     var parsed = BMLoader.parseGithub(file);
-    return fetch(`https://api.github.com/repos/${parsed.slug}/releases/latest`)
-    .then(response => {
+    return fetch(`https://api.github.com/repos/${parsed.slug}/releases/latest`).then(response => {
       if (response.ok) {
-        return response.json()
-        .then(json => `https://cdn.rawgit.com/${parsed.slug}/${json.tag_name}/${parsed.filepath}`);
+        return response.json().then(json => `https://cdn.rawgit.com/${parsed.slug}/${json.tag_name}/${parsed.filepath}`);
       } else {
         throw new Error("Couldn't connect to GitHub");
       }
-    }).catch(alert)
+    }).catch(alert);
   },
   loadGithub: file => {
-    BMLoader.getGithub(file).then(latest =>
-      BMLoader.loadBookmarklet(latest)
-    )
+    BMLoader.getGithub(file).then(latest => BMLoader.loadBookmarklet(latest));
   }
 };
